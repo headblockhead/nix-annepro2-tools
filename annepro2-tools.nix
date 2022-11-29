4a569e699f65178eab459f97c014a72ed2e35710
Edward Hesketh <headblockhead@gmail.com>
2022-11-29 19:53:02 +0000

    updated version and hash

--- annepro2-tools.nix
+++ annepro2-tools.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation {
   pname = "annepro2-tools";
-  version = "4e6e1cebfa4d9f8914d4d22165a346431ea095bb";
+  version = "fe5ed6585b0af274e3220d5abe49ee419c34924a";
 
   src = fetchurl {
-    url = "https://ci.codetector.org/job/OpenAnnePro/job/AnnePro2-Tools/job/master/6/artifact/target/release/annepro2_tools_linux_x64";
-    sha256 = "i1S7bJAg4DsFa/ql8tAs8DmYnn6fIr6oLKPRjvqdkvo=";
+    url = "https://ci.codetector.org/job/OpenAnnePro/job/AnnePro2-Tools/job/master/8/artifact/target/release/annepro2_tools_linux_x64";
+    sha256 = "znOcm02uJyh1AkZHIiYaVUiEzQVinprgTXpYaLdqXNg=";
   };
 
   unpackPhase = ":";
