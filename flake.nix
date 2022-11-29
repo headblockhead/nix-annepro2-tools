2738c843f4aa04d6d8072ebeaa18e906cfb6b55b
Edward Hesketh <headblockhead@gmail.com>
2022-11-29 19:55:14 +0000

    fixed broken flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "A set of tools to flash an annepro2 keyboard with custom firmware.";
   outputs = { self, nixpkgs, flake-utils }:
@@ -9,7 +8,7 @@
         annepro2tools = pkgs.callPackage ./annepro2-tools.nix {};
       in
       {
-        defaultPackage = annepro2tools;
-        packages = annepro2tools;
-      }
+        packages.x86_64-linux.default = annepro2tools;
+        defaultPackage.x86_64-linux = annepro2tools;
+      };
 }
