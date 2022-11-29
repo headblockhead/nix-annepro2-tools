06a59a15100e91ebcfe1bc7a73b2f99a151c9d62
Edward Hesketh <headblockhead@gmail.com>
2022-11-29 19:56:02 +0000

    formatted files

--- annepro2-tools.nix
+++ annepro2-tools.nix
@@ -5,19 +5,16 @@ stdenv.mkDerivation {
   version = "fe5ed6585b0af274e3220d5abe49ee419c34924a";
 
   src = fetchurl {
-    url = "https://ci.codetector.org/job/OpenAnnePro/job/AnnePro2-Tools/job/master/8/artifact/target/release/annepro2_tools_linux_x64";
+    url =
+      "https://ci.codetector.org/job/OpenAnnePro/job/AnnePro2-Tools/job/master/8/artifact/target/release/annepro2_tools_linux_x64";
     sha256 = "znOcm02uJyh1AkZHIiYaVUiEzQVinprgTXpYaLdqXNg=";
   };
 
   unpackPhase = ":";
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
+  nativeBuildInputs = [ autoPatchelfHook ];
 
-  buildInputs = [
-    pkgs.libusb1
-  ];
+  buildInputs = [ pkgs.libusb1 ];
 
   installPhase = ./annepro2-tools-install.sh;
   system = builtins.currentSystem;
--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
 {
-  description = "A set of tools to flash an annepro2 keyboard with custom firmware.";
+  description =
+    "A set of tools to flash an annepro2 keyboard with custom firmware.";
   outputs = { self, nixpkgs, flake-utils }:
-      let 
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-        annepro2tools = pkgs.callPackage ./annepro2-tools.nix {};
-      in
-      {
-        packages.x86_64-linux.default = annepro2tools;
-        defaultPackage.x86_64-linux = annepro2tools;
-      };
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      annepro2tools = pkgs.callPackage ./annepro2-tools.nix { };
+    in {
+      packages.x86_64-linux.default = annepro2tools;
+      defaultPackage.x86_64-linux = annepro2tools;
+    };
 }
