5c2041a946dff6068e85d29e4bc1b1900398028b
Edward Hesketh <headblockhead@gmail.com>
2024-07-09 21:22:15 +0100

    chore: tidy annepro2 installer

--- annepro2-tools.nix
+++ annepro2-tools.nix
@@ -11,14 +11,27 @@ stdenv.mkDerivation {
     owner = "OpenAnnePro";
     repo = "AnnePro2-Tools";
     rev = version;
-    sha256="Bwzxhh4HUDOB+yvtQa0tdPgtVzgdQnrQKeHzObP5ctA=";
+    sha256 = "Bwzxhh4HUDOB+yvtQa0tdPgtVzgdQnrQKeHzObP5ctA=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
 
   buildInputs = [ pkgs.cacert pkgs.libusb1 pkgs.cargo pkgs.rustc pkgs.pkgconfig ];
 
-  installPhase = ./annepro2-tools-install.sh;
+  installPhase = ''
+    # Create the standard environment.
+    source $stdenv/setup
+
+    cp -r $src/* ./
+    cargo build --release
+
+    # Create place to store the binaries.
+    mkdir -p $out/bin
+    # Copy the binary to the output binary directory.
+    cp ./target/release/annepro2_tools $out/bin/annepro2-tools
+    # Allow execution of the binary.
+    chmod +x $out/bin/annepro2-tools
+  '';
   system = builtins.currentSystem;
 }
 
